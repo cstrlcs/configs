1d3914da6f9d117dc8020986c4826123db231b8e
Lucas Castro <contact@lucascastro.dev>
2025-02-07 20:12:28 -0300

    chore: up version

--- apply.sh
+++ apply.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-if ! which jq >/dev/null; then 
+set -e
+
+if ! command -v jq >/dev/null 2>&1; then 
     echo "jq is not installed"
     exit 1
 fi
 
-echo "🚨 This script will add the following dependencies to your Bun project:" 
-echo "  - @cstrlcs/configs"
-echo "  - @biomejs/biome"
-echo "  - typescript"
+cat <<EOF
+🚨 This script will add the following dependencies to your Bun project:
+  - @cstrlcs/configs
+  - @biomejs/biome
+  - typescript
+
+It will also edit/overwrite the following files:
+  - biome.json
+  - tsconfig.json
+  - package.json
+  - .gitattributes
+
+Make sure you have a backup of those files before proceeding.
+EOF
+
+read -rp "Are you sure you want to continue? (y/N) " -n 1 -r REPLY
 echo
 
-echo "It will also edit/overwrite the following files:"
-echo "  - biome.json"
-echo "  - tsconfig.json"
-echo "  - package.json"
-echo "  - .gitattributes"
-
-echo
-echo  "Make sure you have a backup of these files before proceeding."
-echo
-read -p "Are you sure you want to continue? (y/n) " -n 1 -r
-echo
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    bun add -D @cstrlcs/configs @biomejs/biome typescript
-    jq '.scripts |= . + { "lint": "biome check .", "lint:fix": "biome check . --apply-unsafe" }' package.json > package.json.temp && mv package.json.temp package.json
-    
-    echo "{ \"extends\": [\"@cstrlcs/configs/biome\"] }" > biome.json
-    echo "{ \"extends\": \"@cstrlcs/configs/tsconfig\", \"compilerOptions\": { \"baseUrl\": \".\", \"paths\": { \"@/*\": [\"./src/*\"] } }, \"include\": [\"src\"] }" > tsconfig.json
-    echo -e "* text=auto\n*.* text eol=lf" > .gitattributes
-    
-    bunx biome check package.json biome.json tsconfig.json --apply-unsafe
-else
-    echo "Aborting..."
+if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then
+    echo "❌ Aborting..."
+    exit 0
 fi
+
+bun add -D @cstrlcs/configs @biomejs/biome typescript
+jq '.scripts |= . + { "lint": "biome check .", "lint:fix": "biome check . --apply-unsafe" }' package.json > package.json.temp && mv package.json.temp package.json
+
+echo '{ "extends": ["@cstrlcs/configs/biome"] }' > biome.json
+echo '{ "extends": "@cstrlcs/configs/tsconfig", "compilerOptions": { "baseUrl": ".", "paths": { "@/*": ["./src/*"] } }, "include": ["src"] }' > tsconfig.json
+echo -e "* text=auto\n*.* text eol=lf" > .gitattributes
+
+bunx biome check package.json biome.json tsconfig.json --apply-unsafe
